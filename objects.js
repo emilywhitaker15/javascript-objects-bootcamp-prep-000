15de7196014dd66331fe43b39f68698e9e03a3f2
emilywhitaker15@gmail.com <emilywhitaker15@gmail.com>
2018-05-17 16:54:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,6 @@
 var playlist = {
   Ella: 'A-Tisket, A-Tasket',
-  Louis: 'Hello, Dolly',
-  Trane: 'A Love Supreme',
-  Miles: 'Freddie Freeloader'
-}
+}  
 
 function updatePlaylist(playlist, artist, song) {
   return Object.assign({}, playlist, {[artist]: song})
